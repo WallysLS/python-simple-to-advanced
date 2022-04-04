eba1a8a19c8aedb46e4c9606d6e29ff589e5a17e
WallysLS <wallys31.souza@gmail.com>
2022-04-04 10:01:20 -0300

    finished version 1

--- guessing.py
+++ guessing.py
@@ -1,3 +1,17 @@
 # Guess a number
+import random
+
+random_number = random.randint(1,10)
 
 print("This is a guessing game. Write a number ranging from 1 to 10")
+
+
+user_number = input("Write here:")
+
+while(int(user_number) != random_number):
+    print("That's wrong :( Try again")
+    user_number = input("Write here: ")
+    
+else:
+    print("You won :)")
+    
