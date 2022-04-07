194caba4ce435e249304c00a53a512ee6dbd87ec
WallysLS <wallys31.souza@gmail.com>
2022-04-07 12:50:41 -0300

    Updated

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -3,12 +3,21 @@ import random
 print("This is a Rock, Paper, Scissor game! How good are you in this game? ")
 print("In order to play, write rock, paper or scissors")
 
-computer_role = random.choice(["rock","paper","scissor"])
+computer_role = random.choice(["rock","paper","scissors"])
 
 user_role = input("Write here: ")
 user_role = user_role.lower().replace(" ","")
 
+if(user_role != "rock" and user_role != "paper" and user_role != "scissors"):
+    print("I'm sorry, you typed something wrong. The input must be rock, paper or scissors")
 
+
+while(computer_role == "rock"):
+    if(user_role == "rock"):
+        print("The computer also chosed rock, so it's even. Try again!")
+        user_role = input("Write here: ")
+    if(user_role == "paper"):
+        print()
 
 
 
