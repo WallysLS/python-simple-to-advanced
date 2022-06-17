f1d6274d6e377a5baa2da5da5765c5abb1e3f6d1
WallysLS <wallys.souza@gmail.com>
2022-06-17 17:19:51 -0300

    updated

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -3,8 +3,18 @@
 print("Hi, there! This is a guessing game")
 print("Choose r for rock, s for scissors and p for paper")
 
+computer_role = "r"
+"""
 user_role = input("Choose your role: ").lower()
-if (user_role == "r"):
-    print("You chose Rock") 
+if (user_roleu == "r" & computer_role == "p"):
+    print("The computer chose paper! You lost :(")
     
+"""
 
+var1 = input("here: ")
+var2 = "a"
+
+if(var1 == var2):
+    print(type(var1))
+else:
+    print("it didn't work")
