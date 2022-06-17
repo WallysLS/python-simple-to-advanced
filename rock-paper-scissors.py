7c8e059d17d4fdcf52492ba2a0f519bc627b0509
WallysLS <wallys.souza@gmail.com>
2022-06-17 09:34:11 -0300

    first commit

---
+++ rock-paper-scissors.py
@@ -0,0 +1,10 @@
+# A very simple Rock paper scissors game
+
+print("Hi, there! This is a guessing game")
+print("Choose r for rock, s for scissors and p for paper")
+
+user_role = input("Choose your role: ").lower()
+if (user_role == "r"):
+    print("You chose Rock") 
+    
+
