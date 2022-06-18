c9954e0fe7170eeb19a71d86f2f470f00a599c9e
WallysLS <wallys.souza@gmail.com>
2022-06-18 17:37:18 -0300

    updated

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,20 +1,9 @@
-# A very simple Rock paper scissors game
+# A very simple Rock Paper Scissors game
 
-print("Hi, there! This is a guessing game")
-print("Choose r for rock, s for scissors and p for paper")
+import random
 
-computer_role = "r"
-"""
-user_role = input("Choose your role: ").lower()
-if (user_roleu == "r" & computer_role == "p"):
-    print("The computer chose paper! You lost :(")
-    
-"""
+print("Hi, there! This is a Rock Paper Scissor game")
 
-var1 = input("here: ")
-var2 = "a"
+computer_role = random.choice(["rock","paper","scissors"])
+user_role = input("Choose a role: ").lower()
 
-if(var1 == var2):
-    print(type(var1))
-else:
-    print("it didn't work")
